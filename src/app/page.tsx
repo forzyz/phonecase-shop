c3603a8cf08fad8b26c2edeb845fdb3c3ffe2446
Ruslan Telitsyn <zimpuytel@gmail.com>
2024-08-01 12:42:36 +0300

    animated section implementation, tw.conf changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import Reviews from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
@@ -229,8 +230,8 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
 
-        <div>
-          
+        <div className="pt-16">
+          <Reviews />
         </div>
       </section>
     </div>
